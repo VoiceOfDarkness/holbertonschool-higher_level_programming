a96176d2b43304380fe986f42908b2f24e6e44ea
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-22 22:37:16 +0400

    hot fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-for num in range(100):
-    if (num == 99):
-        print(f'{num:02}')
-        break
-    print(f'{num:02}', end=', ')
+for num in range(0, 99):
+    print("{:02d}".format(num), end=", ")
+print("99")
