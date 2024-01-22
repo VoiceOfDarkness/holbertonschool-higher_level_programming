d89bb00a7e2e64618aa00fd07b5755eca23c6c93
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-22 18:25:10 +0400

    hot fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 string = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(string[39:66] + string[106:112] + string[:6])
-print(string)
