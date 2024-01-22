f6b8f5d355c868507f836d5ee32cc63b11dadd0a
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-22 17:37:54 +0400

    all tasks

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3g
+str1 = "Holberton"g
+str2 = "School"g
+str = str1 + " " + str2g
+print(f"Welcome to {str1}!")
---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3g
+word = "Holberton"g
+word_first_3 = word[:3]g
+word_last_2 = word[-2:]g
+middle_word = word[1:1]g
+print(f"First 3 letters: {word_first_3}")g
+print(f"Last 2 letters: {word_last_2}")g
+print(f"Middle word: {middle_word}")
