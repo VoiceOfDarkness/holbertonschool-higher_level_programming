1bd1b4a2531dbd3cb9dfc13cadb8864c6ab242c3
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-25 23:30:05 +0400

    THEY WANT BAD CODE(check previous commit)

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add, div, mul, sub
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    funcs = {"+": add(a, b), "/": div(a, b), "*": mul(a, b), "-": sub(a, b)}
-    for operator, result in funcs.items():
-        print("{} {} {} = {}".format(a, operator , b, result))
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
