4361e13a37de81132d6873701eaae9897dd22569
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-25 13:43:54 +0400

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for let in str:
+        asci_let = ord(let)
+        if (asci_let >= 97 and asci_let <= 122):
+            up_let = chr(asci_let - 32)
+            result += up_let
+        else:
+            result += chr(asci_let)
+    print(result)
