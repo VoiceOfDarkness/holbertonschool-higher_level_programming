918c487e29ed1c9c4a8b01b93c8f2a3740f9630d
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-25 14:12:15 +0400

    hot fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,11 @@ def fizzbuzz():
     for num in range(1, 101):
         fizz = num % 3
         buzz = num % 5
-        if fizz and buzz:
+        if fizz == 0 and buzz == 0:
             print("{}".format('FizzBuzz'), end=' ')
-        elif fizz:
+        elif fizz == 0:
             print("{}".format('Fizz'), end=' ')
-        elif buzz:
+        elif buzz == 0:
             print("{}".format('Buzz'), end=' ')
+        else:
+            print("{}".format(num), end=' ')
