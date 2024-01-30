5da09bb4304d57e0a0dee6382162fed175c281cb
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-30 19:36:22 +0400

    trying push same with hope

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ def add_integer(a, b=98):
     Function adds two integers
     """
     if isinstance(a, (int, float)) is False:
-        raise TypeError("a must be an integer")
+        raise TypeError('a must be an integer')
     if isinstance(b, (int, float)) is False:
-        raise TypeError("b must be an integer")
+            raise TypeError('b must be an integer')
     return int(a) + int(b)
