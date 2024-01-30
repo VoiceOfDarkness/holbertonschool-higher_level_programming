86d2ad234fbc4bf5dd721df6b7924d12c73b681c
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-30 15:41:39 +0400

    hot fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,8 +46,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print("\n" * self.__position[1], end="")
-            output = ""
-            for _ in range(self.__size):
-                output += " " * self.__position[0] + "#" * self.__size + "\n"
-            print(output)
+            for i in range(self.position[1]):
+                print()
+            for j in range(0, self.size):
+                for e in range(self.position[0]):
+                    print(" ", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print()
