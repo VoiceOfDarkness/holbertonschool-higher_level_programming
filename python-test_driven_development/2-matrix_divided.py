4a25a7277144241c7354aaf03538019a01870343
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-01 14:31:44 +0400

    hot fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
 def matrix_divided(matrix: list, div: int):
     listError = 'matrix must be a matrix (list of lists) of integers/floats'
     sizeError = 'Each row of the matrix must have the same size'
     if not isinstance(matrix, (list)):
         raise TypeError(listError)
-    for item in range(len(matrix)):
-        if item != 0:
-            result = item - 1
-            if len(matrix[item]) is not len(matrix[result]):
-                raise TypeError(sizeError)
-    if not isinstance(div, int):
+    for row in matrix:
+        if not all(isinstance(i, (int, float)) for i in row):
+            raise TypeError(listError)
+        if len(row) != len(matrix[0]):
+            raise TypeError(sizeError)
+    if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
     return [[round(i / div, 2) for i in row] for row in matrix]
