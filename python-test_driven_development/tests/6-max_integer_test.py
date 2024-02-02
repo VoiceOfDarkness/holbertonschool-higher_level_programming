4d5c354a081ddc575cc9c35ee336a23f3312d47e
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-02 19:52:30 +0400

    6th task

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        test_list = []
+        self.assertEqual(max_integer(test_list), None)
+
