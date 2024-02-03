ae1f332d26e8131aeb7e6f1cacbd41766496ceb3
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-03 13:43:02 +0400

    additional task 1

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,6 +4,6 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    weight =sum([row[1] for row in my_list])
+    weight = sum([row[1] for row in my_list])
     score = sum(list(map(lambda x: x[0] * x[1], my_list)))
     return (score / weight)
