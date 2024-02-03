0a1a7cf4612b2ea1f8797f8f4780bf98bfc54e8d
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-03 14:55:31 +0400

    bruh

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-
-
 def complex_delete(a_dictionary, value):
-    return {key: val for key, val in a_dictionary.items() if val != value}
+    new_list = []
+    for k, v in a_dictionary.items():
+        if value is v:
+            new_list.append(k)
+    if len(new_list) > 0:
+        for new in new_list:
+            del a_dictionary[new]
+    return a_dictionary
