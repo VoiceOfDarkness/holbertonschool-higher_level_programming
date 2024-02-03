232e0806a0446e94884394a3f17dbf930cc39ffb
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-03 15:06:33 +0400

    hot fix

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -3,9 +3,9 @@ from sys import stderr
 
 
 def safe_function(fct, *args):
-        try:
-            result = fct(*args)
-            return result
-        except Exception as err:
-            print('Exception: {}'.format(err), file=stderr)
-        return None
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err:
+        print('Exception: {}'.format(err), file=stderr)
+    return None
