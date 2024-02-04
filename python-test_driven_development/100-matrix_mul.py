5dda660c292edf324afddaf1abf26d184872b491
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-04 17:03:29 +0400

    finally i fix it

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -12,9 +12,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError('m_a must be a list of lists')
     if not all(isinstance(item, list) for item in m_b):
         raise TypeError('m_b must be a list of lists')
-    if not all(item for item in m_a):
+    if not any(m_a):
         raise ValueError("m_a can't be empty")
-    if not all(item for item in m_b):
+    if not any(m_b):
         raise ValueError("m_b can't be empty")
     if not all(isinstance(i, float) or isinstance(i, int)
                for j in m_a for i in j):
@@ -27,7 +27,7 @@ def matrix_mul(m_a, m_b):
     if not all(len(i) == len(m_b[0]) for i in m_b):
         raise TypeError('each row of m_b must be of the same size')
     if len(m_a[0]) != len(m_b):
-        raise ValueError('m_a and m_b can\'t be multiplied')
+        raise ValueError("m_a and m_b can't be multiplied")
 
     return [[
         sum(a * b for a, b in zip(a_row, b_col))
