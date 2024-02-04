5969e915dd2b3670264c8b093775394c2f0d7697
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-04 17:32:55 +0400

    update method

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,10 +5,5 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Docs for checker"""
-    arr_1 = np.array(m_a)
-    arr_2 = np.array(m_b)
-    try:
-        result = np.dot(arr_1, arr_2)
-    except Exception as e:
-        return e
+    result = np.matmul(m_a, m_b)
     return result
