d47ae65244736821816d6d344399b4ab743e5a59
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-06 18:01:08 +0400

    2nd task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Docs for holberton checker"""
+
+
+class MyList(list):
+    """docs for holberton checker"""
+
+    def print_sorted(self):
+        print(sorted(self))
+        
