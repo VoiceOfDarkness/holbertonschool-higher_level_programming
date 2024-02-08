899a115b44817f34f421646e2319476d9b0e0ae3
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 21:13:05 +0400

    hot fix

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Docs for holberton checker"""
+"""This module contains save_to_json_file function"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Docs for holberton checker"""
+    """save_to_json_file function"""
 
-    with open(filename, 'w') as f:
-        f.write(json.dumps(filename))
+    with open(filename, "w") as file:
+        json_string = json.dumps(my_obj)
+        file.write(json_string)
