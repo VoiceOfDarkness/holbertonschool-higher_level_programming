8d6ee5911e88c8554105db5dc903efe0550abfb6
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 15:12:37 +0400

    7th task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Docs for holberton checker"""
+
+
+class BaseGeometry:
+    """Docs for holberton checker"""
+
+    def area(self):
+        raise NotImplemented("area() is not implemented")
