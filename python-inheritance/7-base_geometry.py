f45b943f7281898c2ce8db3f87b965b06199d0cc
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 17:31:11 +0400

    hot fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Docs for holberton checker"""
+"""This module contains a class BaseGeometry"""
 
 
 class BaseGeometry:
-    """Docs for holberton checker"""
+    """class BaseGeometry, abstract class"""
 
     def area(self):
+        """area - abstract method"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
