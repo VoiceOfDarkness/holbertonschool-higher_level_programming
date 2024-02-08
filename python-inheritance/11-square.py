f1ffcac3ddb17972aa2f3cdea9c4e39499e4f2a6
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 19:21:12 +0400

    12th task

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Docs for holberton checker"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Docs for holberton checkelir"""
+
+    def __init__(self, size) -> None:
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+  
+        self.__size = size
+        
