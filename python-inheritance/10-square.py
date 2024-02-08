55877b1ff7fe5336d1c59e483b772d0899ea9521
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 18:53:20 +0400

    hot fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Docs for holberton checker"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Docs for holberton checker"""
+
     def __init__(self, size) -> None:
         self.integer_validator("size", size)
         self.__size = size
@@ -11,4 +14,4 @@ class Square(Rectangle):
         return f"[Rectangle] {self.__size}/{self.__size}"
 
     def area(self):
-        return (self.__size ** 2)
+        return (self.__size * 2)
