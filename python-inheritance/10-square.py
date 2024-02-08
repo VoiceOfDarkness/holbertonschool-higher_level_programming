a7b66b839f71ac070624e27467c3a5708f31da13
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 19:09:04 +0400

    hot fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,4 +9,4 @@ class Square(Rectangle):
     def __init__(self, size) -> None:
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size 
+        self.__size = size
