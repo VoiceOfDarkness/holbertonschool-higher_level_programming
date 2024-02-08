74b064bc7e9a432d1db17bd904cb4fc88d206066
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 20:09:48 +0400

    3rd task

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Docs for holberton checker"""
+
+
+def append_write(filename="", text=""):
+    """Docs for holberton checker"""
+
+    with open(filename, mode='a+', encoding='utf-8') as f:
+        result = f.write(text)
+    return result
