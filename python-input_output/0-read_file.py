8708bc623c96e0334b896bca585239f6509672e3
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-08 19:45:50 +0400

    hot fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Docs for holberton checker"""
 
 
 def read_file(filename=""):
+    """Docs for holberton checker"""
+
     with open(filename, 'r', encoding='utf-8') as e:
         print(e.read(), end='')
