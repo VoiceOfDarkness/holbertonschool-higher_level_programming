dea90fb05a331afbf6fbd517697d24d847e37c76
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-09 11:28:07 +0400

    hot fix

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Docs from holberton checker"""
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+if __name__ == "__main__ ":
+    try:
+        obj = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        obj = []
+    obj += argv[1:]
+    save_to_json_file(obj, "add_item.json")
