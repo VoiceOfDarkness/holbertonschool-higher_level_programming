80b90d8cde9fca58347cf49ba88e4fd97a9cb429
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-09 19:41:36 +0400

    changed implementation

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python3
-from sys import stdin
 """Docs for holberton checker"""
+import sys
+import signal
 
 
-def print_stats(status_codes, total_size):
-    """Docs for holberton checker"""
+total_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
+
+def print_stats(signal=None, frame=None):
     print("File size: {}".format(total_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+
+
+signal.signal(signal.SIGINT, print_stats)
+
+for line in sys.stdin:
+    try:
+        parts = line.split(" ")
+        size = int(parts[-1])
+        code = int(parts[-2])
+        total_size += size
+        if code in status_codes:
+            status_codes[code] += 1
+    except ValueError:
+        continue
+
+    line_count += 1
+    if line_count % 10 == 0:
+        print_stats()
+
+print_stats()
