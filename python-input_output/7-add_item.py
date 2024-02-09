54111049f538f9df7a93220b4277c60e5f691708
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-09 11:32:33 +0400

    maga fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Docs from holberton checker"""
+"""This module contains script that add all arguments"""
 from sys import argv
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-if __name__ == "__main__ ":
+def main():
+    """main function of programm"""
+
     try:
         obj = load_from_json_file("add_item.json")
     except FileNotFoundError:
         obj = []
     obj += argv[1:]
     save_to_json_file(obj, "add_item.json")
+
+
+if __name__ == "__main__":
+    main()
