68f0b3c5c99e59f9cf9fe37cf7f66e022271d5d8
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-09 17:42:29 +0400

    Additional task 1

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Docs for holberton checker"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Docs for holberton checker"""
+
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    new_lines = []
+    
+    for i in range(len(lines)):
+        new_lines.append(lines[i])
+        if search_string in lines[i]:
+            new_lines.append(new_string)
+
+    with open(filename, "w") as file:
+        file.writelines(new_lines)
