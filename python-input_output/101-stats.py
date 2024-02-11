dcfaa7a7ac98816c1e42eac739f2282957215fb2
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-11 21:02:03 +0400

    change implementation

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,44 +1,46 @@
 #!/usr/bin/python3
 """Docs for holberton checker"""
-import sys
-import signal
 
+
+def print_stats(size, status_codes):
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
-    total_size = 0
-    status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
-    }
-    line_count = 0
+    import sys
 
-    def print_stats(sig=None, frame=None):
-        print(f"File size: {total_size}")
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print(f"{code}: {status_codes[code]}")
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-    signal.signal(signal.SIGINT, print_stats)
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-    for line in sys.stdin:
-        try:
-            parts = line.split(" ")
-            size = int(parts[-1])
-            code = int(parts[-2])
-            total_size += size
-            if code in status_codes:
-                status_codes[code] += 1
-        except (IndexError, ValueError):
-            continue
+            line = line.split()
 
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats()
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-    print_stats()
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
