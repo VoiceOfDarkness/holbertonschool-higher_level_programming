feb3109deea082a7d1a61ea83c2693c33dad3c31
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-11 16:18:33 +0400

    hot fix

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Docs for holberton checker"""
 
 
 def add_attribute(obj, name: str, val: str):
+    """Docs for holberton checker"""
+
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     return setattr(obj, name, val)
