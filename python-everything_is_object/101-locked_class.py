886198bf71ec1c0137df0b7bb063e0d3f2bb9846
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-11 14:44:27 +0400

    hot fix

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -3,6 +3,9 @@
 
 
 class LockedCLass:
-    """Docs for holberton checker"""
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
     __slots__ = ["first_name"]
