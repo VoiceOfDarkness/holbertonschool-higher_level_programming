8012d0af49d33d69959be021403e1c387d69627d
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-11 16:30:43 +0400

    hot fi

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 """Docs for holberton checker"""
-import sys
-import signal
 
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+if __name__ == "__name__":
+    import sys
+    import signal
 
 
-def print_stats(signal=None, frame=None):
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    total_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
 
 
-signal.signal(signal.SIGINT, print_stats)
+    def print_stats(signal=None, frame=None):
+        print("File size: {}".format(total_size))
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] > 0:
+                print("{}: {}".format(code, status_codes[code]))
 
-for line in sys.stdin:
-    try:
-        parts = line.split(" ")
-        size = int(parts[-1])
-        code = int(parts[-2])
-        total_size += size
-        if code in status_codes:
-            status_codes[code] += 1
-    except ValueError:
-        continue
 
-    line_count += 1
-    if line_count % 10 == 0:
-        print_stats()
+    signal.signal(signal.SIGINT, print_stats)
 
-print_stats()
+    for line in sys.stdin:
+        try:
+            parts = line.split(" ")
+            size = int(parts[-1])
+            code = int(parts[-2])
+            total_size += size
+            if code in status_codes:
+                status_codes[code] += 1
+        except ValueError:
+            continue
+
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
+
+    print_stats()
