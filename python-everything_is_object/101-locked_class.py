4fe0dd159958feb98f850e7fe7fa65f25c7a1cae
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-11 14:50:10 +0400

    hot fix

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -7,5 +7,4 @@ class LockedCLass:
     Prevent the user from instantiating new LockedClass attributes
     for anything but attributes called 'first_name'.
     """
-
     __slots__ = ["first_name"]
