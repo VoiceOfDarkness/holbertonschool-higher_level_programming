da5bdcbe4c7c9feaa9cce45ca3afe8903ffdbb4e
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-11 16:45:04 +0400

    hot fix

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """Docs for holberton checker"""
 
+import sys
+import signal
 
-if __name__ == "__name__":
-    import sys
-    import signal
-
-
+if __name__ == "__main__":
     total_size = 0
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     line_count = 0
 
-
-    def print_stats(signal=None, frame=None):
-        print("File size: {}".format(total_size))
+    def print_stats(sig=None, frame=None):
+        print(f"File size: {total_size}")
         for code in sorted(status_codes.keys()):
             if status_codes[code] > 0:
-                print("{}: {}".format(code, status_codes[code]))
-
+                print(f"{code}: {status_codes[code]}")
 
     signal.signal(signal.SIGINT, print_stats)
 
