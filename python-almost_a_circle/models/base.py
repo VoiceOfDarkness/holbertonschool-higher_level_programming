de84bb313866763634ec9718b483b2ecc9bc55a4
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-12 16:43:21 +0400

    15th task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Docs for holberton checker"""
+import json
 
 
 class Base:
@@ -13,3 +14,12 @@ class Base:
         else:
             Base.__np_objects += 1
             self.id = Base.__np_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Docs for holberton checker"""
+
+        if not list_dictionaries and len(list_dictionaries) != 0:
+            return "[]"
+
+        return (json.dumps(list_dictionaries))
