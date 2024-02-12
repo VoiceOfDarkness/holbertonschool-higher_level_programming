a99689f4eb9a908156efe6b8143345f604d5c33d
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-02-12 13:26:06 +0400

    hot fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,10 +38,9 @@ class Rectangle(Base):
         self.__x = x
 
     @property
-    def y(self)
+    def y(self):
         return self.__y
 
     @y.setter
     def y(self, value):
         self.__y = value
-
