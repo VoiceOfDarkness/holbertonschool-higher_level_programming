9b94a399e0a43f21a61e1997ee82debf8dcd1da1
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-18 17:21:56 +0400

    hot fix

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
--- create a table called first_table
-CREATE TABLE IF NOT EXISTS 'first_table'(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  age INT
-);
+-- Creates a table called first_table with values id and name.
+CREATE TABLE
+    IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
