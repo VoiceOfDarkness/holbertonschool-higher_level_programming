619a0abe11863dbc134b9e22df35b01abafb9fa6
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-20 13:25:11 +0400

    hot fix

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,5 @@
 -- script that creates the table force_name
-CREATE TABLE
-    IF NOT EXISTS force_name (id INT PRIMARY KEY, name VARCHAR(100) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+        id INT,
+        name VARCHAR(100) NOT NULL
+);
