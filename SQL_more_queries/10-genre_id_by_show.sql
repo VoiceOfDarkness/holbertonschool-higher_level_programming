999a9f0d002f362e456961ecf4e33e988d589099
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-20 13:57:03 +0400

    hot fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,9 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT
-    tv_show.title,
-    tv_show_genres.genre_id
+    s.title,
+    g.genre_id
 FROM
-    tv_show
-    JOIN genre ON tv_show.genre_id = genre.id
+    tv_show AS s
+    INNER JOIN tv_show_genres AS g ON s.id = g.show_id
 ORDER BY
-    (tv_show.title, tv_show_genres.genre_id) ASC;
+    s.title, g.genre_id;
