91a6788596420adcd5a7ca39562a7f28f7f52426
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-20 18:14:46 +0400

    join fix

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,8 +4,8 @@ SELECT
     g.name
 FROM
     tv_shows AS s
-    INNER JOIN tv_show_genres AS t ON s.id = t.show_id
-    INNER JOIN tv_genres AS g ON t.genre_id = g.id
+    LEFT JOIN tv_show_genres AS t ON s.id = t.show_id
+    LEFT JOIN tv_genres AS g ON t.genre_id = g.id
 ORDER BY
     s.title,
     g.name;
