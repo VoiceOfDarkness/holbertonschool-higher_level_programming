1510ed1a5b8a019e6c673d66a70c8b5475e709f4
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-20 18:13:44 +0400

    17th task

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT
+    s.title,
+    g.name
+FROM
+    tv_shows AS s
+    INNER JOIN tv_show_genres AS t ON s.id = t.show_id
+    INNER JOIN tv_genres AS g ON t.genre_id = g.id
+ORDER BY
+    s.title,
+    g.name;
