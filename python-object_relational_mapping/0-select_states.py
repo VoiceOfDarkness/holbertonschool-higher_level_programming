87c686060e481fcb91ddc914d434ae724b6cb531
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-03-22 15:33:47 +0400

    hot fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 # Lists all states from the database hbtn_0e_0_usa.
-import MySQLdb as ms
-from sys import argv
-
-
-host = "localhost"
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
-
-db = ms.connect(host=host, user=user, passwd=passwd, db=db, port=3306)
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    cur = db.cursor()
-    cur.execute("SELECT * FROM Customers;")
-    
-    print(*cur.fetchall(), sep="\n")
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
